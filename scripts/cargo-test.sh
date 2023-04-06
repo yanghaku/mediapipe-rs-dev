2fd3aa42e3949e49003b2cf81b5bd625ae951c29
yanghaku <1961882079@qq.com>
2023-04-06 21:57:27 +0800

    fix ffmpeg env var
    Signed-off-by: yanghaku <1961882079@qq.com>

--- scripts/cargo-test.sh
+++ scripts/cargo-test.sh
@@ -6,7 +6,7 @@ source "$HOME/.cargo/env"
 
 CURRENT="$(realpath "$(dirname -- "$0")")"
 WASI_SYSROOT="${CURRENT}/../assets/wasi-sysroot"
-INCLUDES="-I${WASI_SYSROOT}/c++ -I${WASI_SYSROOT}/bits -I${WASI_SYSROOT}/wasi"
+INCLUDES="-I${WASI_SYSROOT}/c++/v1 -I${WASI_SYSROOT}/bits -I${WASI_SYSROOT}/wasi"
 export BINDGEN_EXTRA_CLANG_ARGS="--sysroot=${WASI_SYSROOT} --target=wasm32-wasi -fvisibility=default ${INCLUDES}"
 
 # default features (audio,text,vision)
